7f06e79a4585d1dd06a8b9c3545e21635000c208
Tabish Shamim <65093869+Tabz05@users.noreply.github.com>
2023-09-26 16:06:27 +0400

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tictactoepro/firebase_options.dart';
 import 'package:tictactoepro/init_wid.dart';
-import 'package:tictactoepro/screens/agree.dart';
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
@@ -16,49 +13,14 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-
-  Future _userAccepted() async {
-    
-    SharedPreferences sp = await SharedPreferences.getInstance();
-
-    String accept = sp.getString('accept') ?? 'not';
-
-    return accept;
-  }
-
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return FutureBuilder(
-      future: _userAccepted(),
-      builder: (ctx, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return SizedBox();
-          } else if (snapshot.hasData) {
-            if (snapshot.data == 'not') {
-
-              return MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: InitializerWidget(),
-              );
-            } else {
-              return MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home:InitializerWidget(),
-              );
-
-            }
-          } else {
-            return SizedBox();
-          }
-        } else {
-          return SizedBox();
-        }
-      },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InitializerWidget(),
     );
   }
 }
